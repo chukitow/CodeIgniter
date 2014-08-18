b1e6bf6ff82aaadc34636aa5cb29127a5e5e4992
Ivan Velasquez <ivan.velasquez.rios@gmail.com>
2014-08-18 08:56:15 -0500

    Layouts system added

--- system/core/Controller.php
+++ system/core/Controller.php
@@ -48,6 +48,54 @@ class CI_Controller {
 	private static $instance;
 
 	/**
+	 * variable used to pass the data to view
+	 *
+	 * @var	object
+	 */
+	var  $data = array();
+
+	/**
+	 *  layout controller
+	 *
+	 * @var	string
+	 */
+	var  $layout = "application";
+
+	/**
+	 *  ajax methods
+	 *
+	 * @var	string
+	 */
+	var  $ajax = array();
+
+	/**
+	 * remap function
+	 *
+	 * @return	void
+	 */
+	public function _remap($method, $params = array())
+	{
+	    if (method_exists($this, $method)){
+
+
+	        call_user_func_array(array($this, $method), $params);
+	        $this->setLayout($this->layout);
+
+
+
+			if (!in_array($this->router->method, $this->ajax)) {
+				$this->view(strtolower($this->router->class)."/".$this->router->method, $this->data);
+			}
+	    }
+	    else{
+
+	    	show_404();
+	    }
+	    
+	}
+
+
+	/**
 	 * Class constructor
 	 *
 	 * @return	void
@@ -66,6 +114,8 @@ class CI_Controller {
 
 		$this->load =& load_class('Loader', 'core');
 		$this->load->initialize();
+		$mod_path = APPPATH . 'models/';
+		if(file_exists($mod_path)) $this->_read_model_dir($mod_path);
 		log_message('debug', 'Controller Class Initialized');
 	}
 
@@ -81,6 +131,70 @@ class CI_Controller {
 	{
 		return self::$instance;
 	}
+
+	/**
+	 *  Open model directories recursively and load the models inside
+	 *@params dirpath
+	 *dirpath:	model path 
+	 */
+	private function _read_model_dir($dirpath)
+	{
+		$ci =& get_instance();
+
+		$handle = opendir($dirpath);
+		if(!$handle) return;
+
+		while (false !== ($filename = readdir($handle)))
+		{
+			if($filename == "." or $filename == "..") continue;
+
+			$filepath = $dirpath.$filename;
+			if(is_dir($filepath))
+				$this->_read_model_dir($filepath);
+
+			elseif(strpos(strtolower($filename), '.php') !== false)
+			{
+				$name = strtolower($filepath);
+				require_once $name;
+			}
+
+			else continue;
+		}
+
+		closedir($handle);
+	}
+
+	/**
+	 *	set the layout controller
+	 *@params layout
+	 *layout:	the layout name
+	 */
+	private function setLayout($layout)
+    {
+      $this->layout = $layout;
+    }
+
+
+    /**
+	 *	load the view
+	 *@params view
+	 *view:	the view name
+	 */
+    private function view($view, $data=null, $return=false)
+    {
+        $loadedData = array();
+        $loadedData['yield'] = $this->load->view($view,$data,true);
+
+        if($return)
+        {
+            $output = $this->load->view("layouts/".$this->layout, $loadedData, true);
+            return $output;
+        }
+        else
+        {
+            $this->load->view("layouts/".$this->layout, $loadedData, false);
+        }
+    }
 
 }
 
