1aaa07ca515a76c398b475866fe1867c06f9e36d
Ivan Velasquez <ivan.velasquez.rios@gmail.com>
2014-08-28 16:52:52 -0500

    generate controller with views

--- application/controllers/ci.php
+++ application/controllers/ci.php
@@ -2,13 +2,15 @@
 
 class Ci extends CI_Controller {
 
-	var $controllers_path;
-	var $model_path;
+	var $controllers_path = "application/controllers/";
+	var $model_path = "application/models/";
+	var $views_path = "application/views/";
 	var $ajax = array('generate','delte','index');
 
 	function __construct()
 	{
 		parent::__construct();
+		$this->load->helper('inflector');
 	}
 
 
@@ -17,7 +19,30 @@ class Ci extends CI_Controller {
 		$action = $this->_getAtrributes($this->uri->segment_array());
 		if ($action) {
 			
-			
+			switch ($action["type"]) {
+				case "controller":
+
+					if (array_key_exists("functions", $action)) {
+						
+						$this->controller($action["name"] , $action["functions"] );
+					}
+					else{
+						
+						$this->controller($action["name"]);
+					}
+					
+
+					break;
+				
+				case "model":
+					echo "generate model";
+					break;
+
+
+				case "scaffold":
+					echo "generate scaffold";
+					break;
+			}
 
 		}
 	}
@@ -35,12 +60,50 @@ class Ci extends CI_Controller {
 
 	private function controller($name=null , $params = array())
 	{
+		$class = "<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');\nclass  $name extends CI_Controller {\n\n\n";
+		if ($params) {
+			
+			foreach ($params as $key => $function) {
+				
+				$class.="\tfunction $function() \n\t{ \n\t}\n\n";
+
+			}
+		}
+		
+		$class.="}\n/* End of file ci.php */\n/* Location: ./application/controllers/$name.php */";
+
+		if (!file_exists(FCPATH.$this->controllers_path.$name.".php")) {
+			
+			$controller = fopen(FCPATH.$this->controllers_path.$name.".php", "w");
+			fwrite($controller, $class);
+			fclose($controller);
+		}
+
+		$this->views($name , $params);
 
 	}
 
+	private function views($controller , $params = array())
+	{
+		mkdir(FCPATH.$this->views_path.$controller);
+		if ($params) {
+			foreach ($params as $key => $view) {
+				
+				$viewFile = fopen(FCPATH.$this->views_path.$controller."/$view.php", "w");
+				fclose($viewFile);
+
+			}
+		}
+	}
+
+
 	private function model($name=null , $params = array())
 	{
-
+		
+		
+			
+		
+		
 	}
 
 	private function scaffold($name=null , $params = array())
@@ -85,6 +148,7 @@ class Ci extends CI_Controller {
 
 		else {
 
+			//Show Error
 			
 		}
 		return $structure;
